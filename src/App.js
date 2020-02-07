3ab3aea51285be9ce6145382e197aaff152a031a
zotov <zotov@reksoft.ru>
2020-02-07 14:33:50 +0300

    final changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React,{ useState, useEffect } from 'react';
-import './App.css';
-import ListOfItems from './components/List/ListOfItems';
-import { useLocation } from "react-router-dom";
-import LinkToItem from './components/LinkTo/LinkToItem';
 import ReactDOM from 'react-dom';
+import { useLocation } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import ListOfItems from './components/List';
+import LinkToItem from './components/LinkTo';
+import getItems from './GetApi/GetApi';
 import {Context} from './context';
-
-const URL = "http://134.209.138.34/items"
-const URL_ITEM = "http://134.209.138.34/item"
-
-
-const fetchData = (val) => (
-  new Promise(async(resolve, reject) => {
-    try {
-      const res = await fetch(val);
-      resolve(res.json());
-    } catch(err) {
-      reject(err);
-    }
-  })    
-)
+import {URL, URL_ITEM} from './constant';
+import './App.css';
 
 
 const App = () => {
@@ -32,9 +19,8 @@ const App = () => {
   
   const location = useLocation()
 
-
-  useEffect(() =>{
-    fetchData(URL).then((res) => {
+  useEffect(() => {
+    getItems.getAllObjects(URL).then((res) => {
       setPlaceForSale(res);
       changeCondition(true);
     })
@@ -46,7 +32,7 @@ const App = () => {
 
   useEffect(() => {
     if(selectItem){
-      fetchData(`${URL_ITEM}${location.pathname}`).then((res) => {
+      getItems.getCurrentObject(`${URL_ITEM}${location.pathname}`).then((res) => {
         setObject(res);
       })
       .catch((err) => {
@@ -69,7 +55,7 @@ const App = () => {
     <Context.Provider value={{placeForSale, object, handleClick, handelClose}}>
       <div>
         {
-          location.pathname.length === 1 ? <div>
+          location.pathname.length === 1 ? <div className='main'>
             <h1 className='main-title'>Продажа, аренда квартир</h1>
             {
               contentLoading && placeForSale ? 
